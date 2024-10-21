9c87c7b9366f2bb67d2967bb6283b9bc43987a8d
Jealyang <jaelyangchoi@gmail.com>
2024-10-21 22:39:24 +0900

    Event Domain creation

---
+++ src/main/java/my/rest_api/events/EventStatus.java
@@ -0,0 +1,5 @@
+package my.rest_api.events;
+
+public enum EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMEND, CLOSED_ENROLLMENT, STARTED, ENDED
+}
---
+++ src/test/java/my/rest_api/events/EventTest.java
@@ -0,0 +1,32 @@
+package my.rest_api.events;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                .name("Spring REST API")
+                .description("Spring REST API")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+
+    @Test
+    public void javaBean(){
+        Event event = new Event();
+        String name = "Spring REST API";
+        String description = "Spring REST API";
+        event.setName(name);
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
