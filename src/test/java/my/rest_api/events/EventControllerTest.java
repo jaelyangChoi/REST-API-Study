3cbb512f598c077740f01cf149d06bf2d91a93b0
Jealyang <jaelyangchoi@gmail.com>
2024-10-22 21:49:06 +0900

    Add test description annotation

--- src/test/java/my/rest_api/events/EventControllerTest.java
+++ src/test/java/my/rest_api/events/EventControllerTest.java
@@ -1,6 +1,7 @@
 package my.rest_api.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import my.rest_api.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -43,6 +44,7 @@ public class EventControllerTest {
      * 테스트에 요청 값고 넣고 이에 따라 컨트롤러를 수정한다.
      */
     @Test
+    @TestDescription("정상적으로 이벤트를 생성하는 테스트")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("Spring")
@@ -72,6 +74,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
@@ -100,6 +103,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 값이 비어있는 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder().build();
 
@@ -113,6 +117,7 @@ public class EventControllerTest {
     }
 
     @Test
+    @TestDescription("입력 값이 잘못된 경우에 에러가 발생하는 테스트")
     public void createEvent_Bad_Request_Wrong_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("Spring")
