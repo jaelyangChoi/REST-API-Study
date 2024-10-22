e57e93755c4b1d64f3babd35a47fbf62945a79e9
Jealyang <jaelyangchoi@gmail.com>
2024-10-22 20:06:30 +0900

    POST /api/events returns 201

--- src/main/java/my/rest_api/events/Event.java
+++ src/main/java/my/rest_api/events/Event.java
@@ -1,13 +1,16 @@
 package my.rest_api.events;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -21,6 +24,7 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
 
 
--- src/test/java/my/rest_api/events/EventControllerTest.java
+++ src/test/java/my/rest_api/events/EventControllerTest.java
@@ -3,8 +3,10 @@ package my.rest_api.events;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,6 +25,9 @@ public class EventControllerTest {
 
     @Autowired
     MockMvc mockMvc; //가짜 DispatcherServlet. 가짜 요청을 만들어 보내고 응답 확인 가능
+
+    @MockBean
+    EventRepository eventRepository;
 
     @Autowired
     ObjectMapper objectMapper;
@@ -46,6 +51,8 @@ public class EventControllerTest {
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타트업 팩토리")
                 .build();
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
 
         mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON)
