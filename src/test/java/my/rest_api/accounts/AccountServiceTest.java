3a66c6b14855bc3f17e8906203c6d58390f4aec3
Jealyang <jaelyangchoi@gmail.com>
2024-10-27 14:18:03 +0900

    UserNotFound 예외 확인

--- src/test/java/my/rest_api/accounts/AccountServiceTest.java
+++ src/test/java/my/rest_api/accounts/AccountServiceTest.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 
 @RunWith(SpringRunner.class)
@@ -26,7 +28,7 @@ class AccountServiceTest {
     AccountRepository accountRepository;
 
     @Test
-    public void findByUsername(){
+    public void findByUsername() {
         // Given
         String username = "cjl2076@naver.com";
         String password = "jaeryang";
@@ -46,5 +48,16 @@ class AccountServiceTest {
         assertThat(userDetails.getPassword()).isEqualTo(password);
     }
 
+
+    @Test
+    public void findByUsernameFail() {
+        String username = "notFound@naver.com";
+        try {
+            accountService.loadUserByUsername(username);
+            fail("supposed to be fail");
+        } catch (UsernameNotFoundException e) {
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+    }
 
 }
